fe7f9a340e5c75da253920117609a306c6c141f4
codingstuff070 <codingstuff070@gmail.com>
2023-10-02 22:46:50 +0530

    Integrated pdf viewer

--- app/src/main/java/com/example/pdffirebaseapp/PdfViewerActivity.kt
+++ app/src/main/java/com/example/pdffirebaseapp/PdfViewerActivity.kt
@@ -2,10 +2,38 @@ package com.example.pdffirebaseapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
+import com.example.pdffirebaseapp.databinding.ActivityPdfViewerBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.URL
 
 class PdfViewerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPdfViewerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pdf_viewer)
+
+        binding = ActivityPdfViewerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fileName = intent.extras?.getString("fileName")
+        val downloadUrl = intent.extras?.getString("downloadUrl")
+
+        lifecycleScope.launch(Dispatchers.IO) {
+
+            val inputStream = URL(downloadUrl).openStream()
+            withContext(Dispatchers.Main) {
+                binding.pdfView.fromStream(inputStream).onRender { pages, pageWidth, pageHeight ->
+                    if (pages >= 1) {
+                        binding.progressBar.visibility = View.GONE
+                    }
+                }.load()
+            }
+        }
     }
+
 }
